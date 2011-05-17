b0b548a359a9077e9877599c79f97524cbf86b8d
j3frea@gmail.com <j3frea@gmail.com@dac2b9b7-9ab1-ccac-618e-49032a0567d3>
2011-05-17 10:07:20 +0000

    LocationLineEdit now implemented (mainwindow has muck which needs to be sorted out) fancy locator though...

--- trunk/verity/mainwindow.cpp
+++ trunk/verity/mainwindow.cpp
@@ -112,8 +112,352 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
     textsAvailable.append("tisch");
     textsAvailable.append("wlc");
 
+
+
+
+    QVector<int> *bookChapterRange = new QVector<int>;
+    bookChapterRange->append(50);
+    bookChapterRange->append(40);
+    bookChapterRange->append(27);
+    bookChapterRange->append(36);
+    bookChapterRange->append(34);
+    bookChapterRange->append(24);
+    bookChapterRange->append(21);
+    bookChapterRange->append(4);
+    bookChapterRange->append(31);
+    bookChapterRange->append(24);
+    bookChapterRange->append(22);
+    bookChapterRange->append(25);
+    bookChapterRange->append(29);
+    bookChapterRange->append(36);
+    bookChapterRange->append(10);
+    bookChapterRange->append(13);
+    bookChapterRange->append(10);
+    bookChapterRange->append(42);
+    bookChapterRange->append(150);
+    bookChapterRange->append(31);
+    bookChapterRange->append(12);
+    bookChapterRange->append(8);
+    bookChapterRange->append(66);
+    bookChapterRange->append(52);
+    bookChapterRange->append(5);
+    bookChapterRange->append(48);
+    bookChapterRange->append(12);
+    bookChapterRange->append(14);
+    bookChapterRange->append(3);
+    bookChapterRange->append(9);
+    bookChapterRange->append(1);
+    bookChapterRange->append(4);
+    bookChapterRange->append(7);
+    bookChapterRange->append(3);
+    bookChapterRange->append(3);
+    bookChapterRange->append(3);
+    bookChapterRange->append(2);
+    bookChapterRange->append(14);
+    bookChapterRange->append(4);
+    bookChapterRange->append(28);
+    bookChapterRange->append(16);
+    bookChapterRange->append(24);
+    bookChapterRange->append(21);
+    bookChapterRange->append(28);
+    bookChapterRange->append(16);
+    bookChapterRange->append(16);
+    bookChapterRange->append(13);
+    bookChapterRange->append(6);
+    bookChapterRange->append(6);
+    bookChapterRange->append(4);
+    bookChapterRange->append(4);
+    bookChapterRange->append(5);
+    bookChapterRange->append(3);
+    bookChapterRange->append(6);
+    bookChapterRange->append(4);
+    bookChapterRange->append(3);
+    bookChapterRange->append(1);
+    bookChapterRange->append(13);
+    bookChapterRange->append(5);
+    bookChapterRange->append(5);
+    bookChapterRange->append(3);
+    bookChapterRange->append(5);
+    bookChapterRange->append(1);
+    bookChapterRange->append(1);
+    bookChapterRange->append(1);
+    bookChapterRange->append(22);
+    QHash<QString, int> *hash;
+    hash = new QHash<QString, int>;
+
+    hash->insert("genesis", 1   );
+    hash->insert("gen", 1   );
+    hash->insert("exodus", 2    );
+    hash->insert("ex", 2    );
+    hash->insert("leviticus",3    );
+    hash->insert("lev",3    );
+    hash->insert("numbers",4    );
+    hash->insert("num",4    );
+    hash->insert("deuteronomy",5    );
+    hash->insert("deut",5    );
+    hash->insert("joshua",6    );
+    hash->insert("josh",6    );
+    hash->insert("judges",7    );
+    hash->insert("jud",7    );
+    hash->insert("ruth",8    );
+    hash->insert("rut",8    );
+    hash->insert("1 samuel",9    );
+    hash->insert("1sam",9    );
+    hash->insert("2 samuel",10    );
+    hash->insert("2sam",10    );
+    hash->insert("1 kings",11    );
+    hash->insert("1ki",11    );
+    hash->insert("2 kings",12    );
+    hash->insert("2ki",12    );
+    hash->insert("1 chronicles",13    );
+    hash->insert("1ch",13    );
+    hash->insert("2 chronicles",14    );
+    hash->insert("2ch",14    );
+    hash->insert("ezra",15    );
+    hash->insert("ez",15    );
+    hash->insert("nehemiah",16    );
+    hash->insert("neh",16    );
+    hash->insert("esther",17    );
+    hash->insert("est",17    );
+    hash->insert("job",18    );
+    hash->insert("psalms",19    );
+    hash->insert("ps",19    );
+    hash->insert("proverbs",20    );
+    hash->insert("pr",20    );
+    hash->insert("ecclesiastes",21    );
+    hash->insert("ecc",21    );
+    hash->insert("song of songs",22    );
+    hash->insert("ss",22    );
+    hash->insert("isaiah",23    );
+    hash->insert("is",23    );
+    hash->insert("jeremiah",24    );
+    hash->insert("jer",24    );
+    hash->insert("lamentations",25    );
+    hash->insert("lam",25    );
+    hash->insert("ezekiel",26    );
+    hash->insert("ez",26    );
+    hash->insert("daniel",27    );
+    hash->insert("dan",27    );
+    hash->insert("hosea",28    );
+    hash->insert("hos",28    );
+    hash->insert("joel",29    );
+    hash->insert("amos",30    );
+    hash->insert("obadiah",31    );
+    hash->insert("ob",31    );
+    hash->insert("jonah",32    );
+    hash->insert("micah",33    );
+    hash->insert("mic",33    );
+    hash->insert("nahum",34    );
+    hash->insert("nah",34    );
+    hash->insert("habakkuk",35    );
+    hash->insert("hab",35    );
+    hash->insert("zephaniah",36    );
+    hash->insert("zep",36    );
+    hash->insert("haggai",37    );
+    hash->insert("hag",37    );
+    hash->insert("zechariah",38    );
+    hash->insert("zec",38    );
+    hash->insert("malachi",39    );
+    hash->insert("mal",39    );
+
+
+    hash->insert( "matthew" ,  40 );
+    hash->insert( "matt" ,  40 );
+    hash->insert( "mat" ,  40 );
+    hash->insert( "mt" ,  40 );
+    hash->insert( "mark" ,  41 );
+    hash->insert( "mrk" ,  41 );
+    hash->insert( "mk" ,  41 );
+    hash->insert( "mr" ,  41 );
+    hash->insert( "luke" ,  42 );
+    hash->insert( "luk" ,  42 );
+    hash->insert( "lk" ,  42 );
+    hash->insert( "john" ,  43 );
+    hash->insert( "joh" ,  43 );
+    hash->insert( "jn" ,  43 );
+    hash->insert( "jhn" ,  43 );
+    hash->insert( "acts" ,  44 );
+    hash->insert( "ac" ,  44 );
+    hash->insert( "romans" ,  45 );
+    hash->insert( "rom" ,  45 );
+    hash->insert( "ro" ,  45 );
+    hash->insert( "rm" ,  45 );
+    hash->insert( "1 corinthians" ,  46 );
+    hash->insert( "1 cor" ,  46 );
+    hash->insert( "1 co" ,  46 );
+    hash->insert( "i co" ,  46 );
+    hash->insert( "1co" ,  46 );
+    hash->insert( "i cor" ,  46 );
+    hash->insert( "1cor" ,  46 );
+    hash->insert( "i corinthians" ,  46 );
+    hash->insert( "1corinthians" ,  46 );
+    hash->insert( "1st corinthians" ,  46 );
+    hash->insert( "first corinthians" ,  46 );
+    hash->insert( "2 corinthians" ,  47 );
+    hash->insert( "2 cor" ,  47 );
+    hash->insert( "2 co" ,  47 );
+    hash->insert( "ii co" ,  47 );
+    hash->insert( "2co" ,  47 );
+    hash->insert( "ii cor" ,  47 );
+    hash->insert( "2cor" ,  47 );
+    hash->insert( "ii corinthians" ,  47 );
+    hash->insert( "2corinthians" ,  47 );
+    hash->insert( "2nd corinthians" ,  47 );
+    hash->insert( "second corinthians" ,  47 );
+    hash->insert( "galatians" ,  48 );
+    hash->insert( "gal" ,  48 );
+    hash->insert( "ga" ,  48 );
+    hash->insert( "ephesians" ,  49 );
+    hash->insert( "ephes" ,  49 );
+    hash->insert( "eph" ,  49 );
+    hash->insert( "philippians" ,  50 );
+    hash->insert( "phil" ,  50 );
+    hash->insert( "php" ,  50 );
+    hash->insert( "colossians" ,  51 );
+    hash->insert( "col" ,  51 );
+    hash->insert( "1 thessalonians" ,  52 );
+    hash->insert( "1 thess" ,  52 );
+    hash->insert( "1 thes" ,  52 );
+    hash->insert( "1 th" ,  52 );
+    hash->insert( "i th" ,  52 );
+    hash->insert( "1th" ,  52 );
+    hash->insert( "i thes" ,  52 );
+    hash->insert( "1thes" ,  52 );
+    hash->insert( "i thess" ,  52 );
+    hash->insert( "1thess" ,  52 );
+    hash->insert( "i thessalonians" ,  52 );
+    hash->insert( "1thessalonians" ,  52 );
+    hash->insert( "1st thessalonians" ,  52 );
+    hash->insert( "first thessalonians" ,  52 );
+    hash->insert( "2 thessalonians" ,  53 );
+    hash->insert( "2 thess" ,  53 );
+    hash->insert( "2 th" ,  53 );
+    hash->insert( "ii th" ,  53 );
+    hash->insert( "2th" ,  53 );
+    hash->insert( "ii thes" ,  53 );
+    hash->insert( "2thes" ,  53 );
+    hash->insert( "ii thess" ,  53 );
+    hash->insert( "2thess" ,  53 );
+    hash->insert( "ii thessalonians" ,  53 );
+    hash->insert( "2thessalonians" ,  53 );
+    hash->insert( "2nd thessalonians" ,  53 );
+    hash->insert( "second thessalonians" ,  53 );
+    hash->insert( "1 timothy" ,  54 );
+    hash->insert( "1 tim" ,  54 );
+    hash->insert( "1 ti" ,  54 );
+    hash->insert( "i ti" ,  54 );
+    hash->insert( "1ti" ,  54 );
+    hash->insert( "i tim" ,  54 );
+    hash->insert( "1tim" ,  54 );
+    hash->insert( "i timothy" ,  54 );
+    hash->insert( "1timothy" ,  54 );
+    hash->insert( "1st timothy" ,  54 );
+    hash->insert( "first timothy" ,  54 );
+    hash->insert( "2 timothy" ,  55 );
+    hash->insert( "2 tim" ,  55 );
+    hash->insert( "2 ti" ,  55 );
+    hash->insert( "ii ti" ,  55 );
+    hash->insert( "2ti" ,  55 );
+    hash->insert( "ii tim" ,  55 );
+    hash->insert( "2tim" ,  55 );
+    hash->insert( "ii timothy" ,  55 );
+    hash->insert( "2timothy" ,  55 );
+    hash->insert( "2nd timothy" ,  55 );
+    hash->insert( "second timothy" ,  55 );
+    hash->insert( "titus" ,  56 );
+    hash->insert( "tit" ,  56 );
+    hash->insert( "philemon" ,  57 );
+    hash->insert( "philem" ,  57 );
+    hash->insert( "phm" ,  57 );
+    hash->insert( "hebrews" ,  58 );
+    hash->insert( "heb" ,  58 );
+    hash->insert( "james" ,  59 );
+    hash->insert( "jas" ,  59 );
+    hash->insert( "jm" ,  59 );
+    hash->insert( "1 peter" ,  60 );
+    hash->insert( "1 pet" ,  60 );
+    hash->insert( "1 pe" ,  60 );
+    hash->insert( "i pe" ,  60 );
+    hash->insert( "1pe" ,  60 );
+    hash->insert( "i pet" ,  60 );
+    hash->insert( "1pet" ,  60 );
+    hash->insert( "i pt" ,  60 );
+    hash->insert( "1 pt" ,  60 );
+    hash->insert( "1pt" ,  60 );
+    hash->insert( "i peter" ,  60 );
+    hash->insert( "1peter" ,  60 );
+    hash->insert( "1st peter" ,  60 );
+    hash->insert( "first peter" ,  60 );
+    hash->insert( "2 peter" ,  61 );
+    hash->insert( "2 pet" ,  61 );
+    hash->insert( "2 pe" ,  61 );
+    hash->insert( "ii pe" ,  61 );
+    hash->insert( "2pe" ,  61 );
+    hash->insert( "ii pet" ,  61 );
+    hash->insert( "2pet" ,  61 );
+    hash->insert( "ii pt" ,  61 );
+    hash->insert( "2 pt" ,  61 );
+    hash->insert( "2pt" ,  61 );
+    hash->insert( "ii peter" ,  61 );
+    hash->insert( "2peter" ,  61 );
+    hash->insert( "2nd peter" ,  61 );
+    hash->insert( "second peter" ,  61 );
+    hash->insert( "1 john" ,  62 );
+    hash->insert( "1 jn" ,  62 );
+    hash->insert( "i jn" ,  62 );
+    hash->insert( "1jn" ,  62 );
+    hash->insert( "i jo" ,  62 );
+    hash->insert( "1jo" ,  62 );
+    hash->insert( "i joh" ,  62 );
+    hash->insert( "1joh" ,  62 );
+    hash->insert( "i jhn" ,  62 );
+    hash->insert( "1 jhn" ,  62 );
+    hash->insert( "1jhn" ,  62 );
+    hash->insert( "i john" ,  62 );
+    hash->insert( "1john" ,  62 );
+    hash->insert( "1st john" ,  62 );
+    hash->insert( "first john" ,  62 );
+    hash->insert( "2 john" ,  63 );
+    hash->insert( "2 jn" ,  63 );
+    hash->insert( "ii jn" ,  63 );
+    hash->insert( "2jn" ,  63 );
+    hash->insert( "ii jo" ,  63 );
+    hash->insert( "2jo" ,  63 );
+    hash->insert( "ii joh" ,  63 );
+    hash->insert( "2joh" ,  63 );
+    hash->insert( "ii jhn" ,  63 );
+    hash->insert( "2 jhn" ,  63 );
+    hash->insert( "2jhn" ,  63 );
+    hash->insert( "ii john" ,  63 );
+    hash->insert( "2john" ,  63 );
+    hash->insert( "2nd john" ,  63 );
+    hash->insert( "second john" ,  63 );
+    hash->insert( "3 john" ,  64 );
+    hash->insert( "3 jn" ,  64 );
+    hash->insert( "iii jn" ,  64 );
+    hash->insert( "3jn" ,  64 );
+    hash->insert( "iii jo" ,  64 );
+    hash->insert( "3jo" ,  64 );
+    hash->insert( "iii joh" ,  64 );
+    hash->insert( "3joh" ,  64 );
+    hash->insert( "iii jhn" ,  64 );
+    hash->insert( "3 jhn" ,  64 );
+    hash->insert( "3jhn" ,  64 );
+    hash->insert( "iii john" ,  64 );
+    hash->insert( "3john" ,  64 );
+    hash->insert( "3rd john" ,  64 );
+    hash->insert( "third john" ,  64 );
+    hash->insert( "jude" ,  65 );
+    hash->insert( "jud" ,  65 );
+    hash->insert( "revelation" ,  66);
+    hash->insert( "rev" ,  66 );
+    hash->insert( "re" ,  66 );
+    QStringList *books = new QStringList();
+    *books << "Genesis" << "Exodus" << "Leviticus" << "Numbers" << "Deuteronomy" << "Joshua" << "Judges" << "Ruth" << "1 Samuel" << "2 Samuel" << "1 Kings" << "2 Kings" << "1 Chronicles" << "2 Chronicles" << "Ezra" << "Nehemiah" << "Esther" << "Job" << "Psalms" << "Proverbs" << "Ecclesiastes" << "Song of Songs" << "Isaiah" << "Jeremiah" << "Lamentations" << "Ezekiel" << "Daniel" << "Hosea" << "Joel" << "Amos" << "Obadiah" << "Jonah" << "Micah" << "Nahum" << "Habakkuk" << "Zephaniah" << "Haggai" << "Zechariah" << "Malachi";
+    *books << "Matthew" << "Mark" << "Luke" << "John" << "Acts" << "Romans" << "1 Corinthians" << "2 Corinthians" << "Galatians" << "Ephesians" << "Philippians" << "Colossians" << "1 Thessalonians" << "2 Thessalonians" << "1 Timothy" << "2 Timothy" << "Titus" << "Philemon" << "Hebrews" << "James" << "1 Peter" << "2 Peter" << "1 John" << "2 John" << "3 John" << "Jude" << "Revelation";
+    qDebug() << books->at(0);
     //must be initialised before the toggleButtons get toggled since it receives focus whenever they're toggled
-    verseLineEdit = new QLineEdit();
+    verseLineEdit = new LocationLineEdit(books, hash, bookChapterRange, 15, this);
     verseLineEdit->setMaximumWidth(300);
 
     for(int i=0; i< textsAvailable.size(); i++)
@@ -164,28 +508,12 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
 
     verseLineOutput = new QLabel();
 
-    QComboBox *cmb = new QComboBox(this);
-    cmb->setEditable(true);
-    QStringList books;
-    books << "Genesis" << "Exodus" << "Leviticus" << "Numbers" << "Deuteronomy" << "Joshua" << "Judges" << "Ruth" << "1 Samuel" << "2 Samuel" << "1 Kings" << "2 Kings" << "1 Chronicles" << "2 Chronicles" << "Ezra" << "Nehemiah" << "Esther" << "Job" << "Psalms" << "Proverbs" << "Ecclesiastes" << "Song of Songs" << "Isaiah" << "Jeremiah" << "Lamentations" << "Ezekiel" << "Daniel" << "Hosea" << "Joel" << "Amos" << "Obadiah" << "Jonah" << "Micah" << "Nahum" << "Habakkuk" << "Zephaniah" << "Haggai" << "Zechariah" << "Malachi";
-    books << "Matthew" << "Mark" << "Luke" << "John" << "Acts" << "Romans" << "1 Corinthians" << "2 Corinthians" << "Galatians" << "Ephesians" << "Philippians" << "Colossians" << "1 Thessalonians" << "2 Thessalonians" << "1 Timothy" << "2 Timothy" << "Titus" << "Philemon" << "Hebrews" << "James" << "1 Peter" << "2 Peter" << "1 John" << "2 John" << "3 John" << "Jude" << "Revelation";
-
-    cmb->addItems(books);
-    QCompleter *completer = new QCompleter(this);
-    completer->setCompletionMode(QCompleter::UnfilteredPopupCompletion); // always show all completions
-    ReferenceFilterProxyModel *pFilterModel = new ReferenceFilterProxyModel(this);
-    pFilterModel->setSourceModel(cmb->model());
-    connect(cmb, SIGNAL(editTextChanged(const QString &)), pFilterModel, SLOT(setFilterFixedString(const QString &)));
-    completer->setModel(pFilterModel); // use the filtered proxy model to do the substring matching
-    cmb->setCompleter(completer);
-
 
     connect(verseLineEdit, SIGNAL(textEdited(QString)), this, SLOT(verseLineEditChanged(QString)));
     connect(verseLineEdit, SIGNAL(returnPressed()), this, SLOT(performVerseLineEdit()));
 
     toolbar->addWidget(verseLineEdit);
     toolbar->addWidget(verseLineOutput);
-    toolbar->addWidget(cmb);
 
 
     addToolBar(toolbar);
@@ -220,6 +548,8 @@ void MainWindow::afterShown()
 void MainWindow::writeOutSettings()
 {
     browser->writeOutSettings();
+
+    qDebug() << this->browser->chapterDisplayer->getCurrentChapter();
 
     QSettings settings(PROGRAM_NAME, PROGRAM_NAME);
 
@@ -295,12 +625,34 @@ void MainWindow::verseLineEditChanged(QString string)
         }
         else
         {
-           verseLineOutput->setText(VerseReferenceParser::parse(string).stringRepresentation);
+            //this is what used to be here:
+//           verseLineOutput->setText(VerseReferenceParser::parse(string).stringRepresentation);
+
+           /*
+    //this code is ripped from the CompleteLineEdit but it could be useful here
+
+    QStringList words = QStringList()  << "Genesis" << "Exodus" << "Leviticus" << "Numbers" << "Deuteronomy" << "Joshua" << "Judges" << "Ruth" << "1 Samuel" << "2 Samuel" << "1 Kings" << "2 Kings" << "1 Chronicles" << "2 Chronicles" << "Ezra" << "Nehemiah" << "Esther" << "Job" << "Psalms" << "Proverbs" << "Ecclesiastes" << "Song of Songs" << "Isaiah" << "Jeremiah" << "Lamentations" << "Ezekiel" << "Daniel" << "Hosea" << "Joel" << "Amos" << "Obadiah" << "Jonah" << "Micah" << "Nahum" << "Habakkuk" << "Zephaniah" << "Haggai" << "Zechariah" << "Malachi";
+    words << "Matthew" << "Mark" << "Luke" << "John" << "Acts" << "Romans" << "1 Corinthians" << "2 Corinthians" << "Galatians" << "Ephesians" << "Philippians" << "Colossians" << "1 Thessalonians" << "2 Thessalonians" << "1 Timothy" << "2 Timothy" << "Titus" << "Philemon" << "Hebrews" << "James" << "1 Peter" << "2 Peter" << "1 John" << "2 John" << "3 John" << "Jude" << "Revelation";
+
+    QRegExp rx = QRegExp("^(\\d\\ )?[a-zA-Z]*\\b");
+    rx.exactMatch(string);
+    if (words.contains(rx.cap(0)))
+    {
+        QString book = rx.cap(0);
+        QString textWithoutBook = string.mid(book.length());
+        rx = QRegExp("^\\ ?(\\d{1,3})\\D(\\d{1,3})$");
+        rx.exactMatch(textWithoutBook);
+        qDebug() << (rx.cap(1).length() > 0 && rx.cap(2) > 0);
+        if (rx.cap(1).length() > 0 && rx.cap(2) > 0)
+        {
+            verseLineOutput->setText("Destination: " + book + " " + rx.cap(1) + ":" + rx.cap(2));
+        }
+    }
+*/
         }
     }
     else
     {
         verseLineOutput->setText("");
     }
-
 }
