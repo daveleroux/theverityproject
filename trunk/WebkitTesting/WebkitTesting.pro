a4c8e7877bbfee6694710b5da4050cba3018beea
daveleroux <daveleroux@dac2b9b7-9ab1-ccac-618e-49032a0567d3>
2011-07-13 18:47:07 +0000

    webkit playground

---
+++ trunk/WebkitTesting/WebkitTesting.pro
@@ -0,0 +1,23 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2011-06-29T18:56:27
+#
+#-------------------------------------------------
+
+QT       += core gui webkit sql xml xmlpatterns
+
+TARGET = WebkitTesting
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp \
+    clicklistener.cpp
+
+HEADERS  += mainwindow.h \
+    clicklistener.h
+
+FORMS    +=
+
+RESOURCES += \
+    application.qrc
---
+++ trunk/WebkitTesting/mainwindow.cpp
@@ -0,0 +1,115 @@
+#include "mainwindow.h"
+#include <QtWebKit/QWebView>
+#include <QtWebKit/QWebFrame>
+#include <QFile>
+#include <QTextStream>
+#include "clicklistener.h"
+#include <QMessageBox>
+#include <QtSql>
+#include <QXmlQuery>
+
+QString MainWindow::getHtml(int v)
+{
+
+    QSqlQuery query;
+
+    if(v==0)
+    {
+        if(!query.exec("select text from net_bible where normalised_chapter=973"))
+        {
+            qDebug() << "failed: " << query.lastError() << endl;
+            exit(1);
+        }
+    }
+    else
+    {
+        if(!query.exec("select text from net_bible where normalised_chapter=58 and verse="+QString().setNum(v) ))
+        {
+            qDebug() << "failed: " << query.lastError() << endl;
+            exit(1);
+        }
+    }
+
+    QString wholeChapter = "<normalisedChapter>";
+    while(query.next())
+    {
+        QString text = query.value(0).toString();
+        wholeChapter = wholeChapter + text;
+    }
+    wholeChapter = wholeChapter + "</normalisedChapter>";
+
+//    qDebug() << wholeChapter;
+
+
+    QXmlQuery xmlQuery(QXmlQuery::XSLT20);
+
+    QByteArray byteArray(wholeChapter.toUtf8());
+
+    QBuffer buffer(&byteArray);
+    buffer.open(QBuffer::ReadWrite);
+
+    xmlQuery.setFocus(&buffer);
+
+    xmlQuery.setQuery(QUrl("bible.xsl"));
+
+    QString result;
+    xmlQuery.evaluateTo(&result);
+
+    return result;
+}
+
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
+{
+
+    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName("verity.sqlite");
+    if (!db.open())
+    {
+        qDebug() << "couldn't open db" << endl;
+        exit(1);
+    }
+
+
+
+
+    webView = new QWebView();
+    setCentralWidget(webView);
+
+
+    connect(webView, SIGNAL(loadFinished(bool)), this, SLOT(loadFinished(bool)));
+
+    //webView->settings()->setAttribute(QWebSettings::JavascriptEnabled,true);
+
+    //    QString html = "<html><head></head><body><h1>hello world</h1></body></html>";
+//    for(int i=1; i<= 32; i++ )
+//    {
+//        qDebug() << i;
+//        getHtml(i);
+//    }
+    QString html = getHtml();
+    qDebug() << html;
+    webView->setHtml(html, QUrl::fromLocalFile(QDir::currentPath()));
+
+    //    webView->load(QUrl("http://qt.nokia.com/"));
+    //         webView->show();
+}
+
+void MainWindow::wordClicked(int id)
+{
+    QMessageBox msgBox;
+    msgBox.setSizeGripEnabled(true);
+
+    msgBox.setText(QString().setNum(id) );
+    msgBox.exec();
+}
+
+MainWindow::~MainWindow()
+{
+
+}
+
+void MainWindow::loadFinished(bool b)
+{
+    ClickListener* clickListener = new ClickListener(this);
+    webView->page()->mainFrame()->addToJavaScriptWindowObject("clickListener", clickListener);
+}
