e856006cd648ebfb246566c202298824d112871d
daveleroux <daveleroux@dac2b9b7-9ab1-ccac-618e-49032a0567d3>
2011-07-13 22:28:28 +0000

    playing around with continuous scrolling

--- WebkitTesting/mainwindow.h
+++ WebkitTesting/mainwindow.h
@@ -13,9 +13,15 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     void wordClicked(int id);
     ~MainWindow();
-    QString getHtml();
+    QString getHtml(int normalisedChapter);
+    void display(int normalisedChapter);
+    void append(int normalisedChapter, QString html);
+    void append();
+    QList<int> normalisedChapters;
+
 public slots:
     void loadFinished(bool b);
+    void scrolled();
 };
 
 #endif // MAINWINDOW_H
